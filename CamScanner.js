ad6b62e23fa84422826098c2e918999e98612c58
Game-2020 <78715190+Game-2020@users.noreply.github.com>
2021-03-06 13:21:59 +0800

    Update CamScanner.js

--- CamScanner.js
+++ CamScanner.js
@@ -14,5 +14,5 @@ QX & Q & Surge Mitm = ap*.intsig.net,
 */
 
 let obj = JSON.parse($response.body);
-obj = {"data":{"psnl_vip_property":{"expiry":"1643731200"}}};
+obj = {"data":{"psnl_vip_property":{"expiry":"7666655555"}}};
 $done({body: JSON.stringify(obj)});
