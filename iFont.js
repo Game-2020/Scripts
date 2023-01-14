86dfc35cd211387038d1e94594aa8bac8fa127c6
Game-2020 <78715190+Game-2020@users.noreply.github.com>
2023-01-14 09:18:36 +0800

    Update iFont.js

--- iFont.js
+++ iFont.js
@@ -2,7 +2,7 @@
 [mitm]
 hostname = app.ilovechina.tech
 [rewrite_local]
-^https:\/\/app\.ilovechina\.tech\/api\.php? url script-response-body https://raw.githubusercontent.com/Game-2020/Scripts/main/iFont.js
+^https:\/\/app\.ilovechina\.tech\/api\.php url script-response-body https://raw.githubusercontent.com/Game-2020/Scripts/main/iFont.js
 */
 
 var body=$response.body;
