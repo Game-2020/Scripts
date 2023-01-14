2b475091d1fd940a30523717dd3fb3a41daec121
Game-2020 <78715190+Game-2020@users.noreply.github.com>
2023-01-14 08:46:52 +0800

    Update iFont.js

--- iFont.js
+++ iFont.js
@@ -7,5 +7,5 @@ hostname = app.ilovechina.tech
 
 var body=$response.body;
 body = body.replace(/"endtime\":"2023-01-14"/g,'"endtime":"2222-12-12"');
-body = body.replace(/"state\":"1"/g,'"state":"1"');
+body = body.replace(/"state\":"0"/g,'"state":"1"');
 $done(body);
