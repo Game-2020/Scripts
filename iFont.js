f41a7b048778979b3cdc21a5b7cff7796a8fe335
Game-2020 <78715190+Game-2020@users.noreply.github.com>
2023-01-14 09:40:46 +0800

    Update iFont.js

--- iFont.js
+++ iFont.js
@@ -5,7 +5,6 @@ hostname = app.ilovechina.tech
 ^https:\/\/app\.ilovechina\.tech\/api\.php url script-response-body https://raw.githubusercontent.com/Game-2020/Scripts/main/iFont.js
 */
 
-var body=$response.body;
-body = body.replace(/"state\":0,'"state":1,');
-body = body.replace(/"endtime\":null/g,'"endtime":"2222-12-12"');
-$done(body);
+var obj = JSON.parse($response.body);
+    obj.state= "2222-12-12";
+    $done({body: JSON.stringify(obj)});
